035b0fc41287791e80000788dc0c6e75b87cc2b4
Mikhail Savichev <mikhail@savichev.pro>
2019-03-11 17:32:58 +0300

    Changed build fingerprint
    Change-Id: Ie01b38011dd32e1391e0e1000a68ba242ec95e50

--- lineage_kenzo.mk
+++ lineage_kenzo.mk
@@ -33,9 +33,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V9.6.2.0.MHOMIFD release-keys"
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V10.2.1.0.MHOMIXM release-keys"
 
-BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V9.6.2.0.MHOMIFD:user/release-keys
+BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V10.2.1.0.MHOMIXM:user/release-keys
 
 # Product packages
 TARGET_USE_JELLY := true
